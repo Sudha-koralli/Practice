a9529a63eb1839ed317b11e6a4a9428c4e25b662
Koralli <sudha.koralli@dxc.com>
2024-03-10 21:46:28 +0530

    online_test_app

---
+++ Online_Test_App/src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { QuestionComponent } from './question/question.component'; 
+
+const routes: Routes = [
+  {
+    path:'', redirectTo:'welcome', pathMatch:'full'
+  },
+  {
+    path:"welcome",
+    component:WelcomeComponent
+  },
+  {
+    path:"Question",
+    component:QuestionComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { 
+  
+}
